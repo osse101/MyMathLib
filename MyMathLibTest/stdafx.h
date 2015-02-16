50a6172b18908d4d1405d057049042050664a520
Michael McKenzie <mmckenzie@mun.ca>
2015-02-16 15:38:07 -0330

    Recompiled gtest

--- MyMathLibTest/stdafx.h
+++ MyMathLibTest/stdafx.h
@@ -4,6 +4,16 @@
 
 #define _VARIADIC_MAX 10
 
+#if _MSC_VER >= 1700
+#pragma comment(lib, "gtestx64.lib")
+#pragma comment(lib, "gtestdx64.lib")
+//#pragma comment(lib, "gtest_mainx64.lib")
+//#pragma comment(lib, "gtest_maindx64.lib")
+#elif _MSC_VER >= 1600
+#pragma comment(lib, "gtestdx86.lib")
+#endif
+
+
 #include <iostream>
 #include <gtest\gtest.h>
 
